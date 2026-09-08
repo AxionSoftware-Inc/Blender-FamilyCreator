b1638781e293c815211dceded176baa047e2735a
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-08 23:53:51 +0500

    Expose semantic member classification API

--- family_types/__init__.py
+++ family_types/__init__.py
@@ -1,4 +1,11 @@
 from .registry import FAMILY_TYPES, family_type_items, get_family_type
-from .strategies import infer_member_rules
+from .strategies import classify_member_role, infer_member_rule, infer_member_rules
 
-__all__ = ["FAMILY_TYPES", "family_type_items", "get_family_type", "infer_member_rules"]
+__all__ = [
+    "FAMILY_TYPES",
+    "family_type_items",
+    "get_family_type",
+    "classify_member_role",
+    "infer_member_rule",
+    "infer_member_rules",
+]
