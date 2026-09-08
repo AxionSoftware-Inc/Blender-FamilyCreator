b469a0bec1e6ecf92672062ad541edc201f50774
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-08 23:46:47 +0500

    Give doors dedicated semantic member roles

--- family_types/door.py
+++ family_types/door.py
@@ -1,5 +1,13 @@
+from .opening_base import classify_role as _classify_opening_role
 from .opening_base import infer_rule as _opening_rule
 
 
-def infer_rule(axis, span_ratio, center_ratio, name=""):
-    return _opening_rule(axis, span_ratio, center_ratio, name)
+ROLE_DOOR_LEAF = "DOOR_LEAF"
+
+
+def classify_role(spans, centers, name=""):
+    return _classify_opening_role(spans, centers, name=name, panel_role=ROLE_DOOR_LEAF)
+
+
+def infer_rule(axis, span_ratio, center_ratio, name="", role=None):
+    return _opening_rule(axis, span_ratio, center_ratio, name=name, role=role)
