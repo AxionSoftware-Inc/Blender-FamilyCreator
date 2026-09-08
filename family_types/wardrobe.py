1c49c5b9d58a3979cfa2c84006ceddb1491278c4
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-08 23:51:10 +0500

    Wire wardrobe to semantic casework roles

--- family_types/wardrobe.py
+++ family_types/wardrobe.py
@@ -1,5 +1,17 @@
+from .casework_base import classify_role as _classify_casework_role
 from .casework_base import infer_rule as _casework_rule
 
 
-def infer_rule(axis, span_ratio, center_ratio, name=""):
-    return _casework_rule(axis, span_ratio, center_ratio, name, editable_axes=("X", "Y", "Z"))
+def classify_role(spans, centers, name=""):
+    return _classify_casework_role(spans, centers, name=name)
+
+
+def infer_rule(axis, span_ratio, center_ratio, name="", role=None):
+    return _casework_rule(
+        axis,
+        span_ratio,
+        center_ratio,
+        name=name,
+        editable_axes=("X", "Y", "Z"),
+        role=role,
+    )
