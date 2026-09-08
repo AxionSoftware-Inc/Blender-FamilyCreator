355364b7554d307a07eda8e16e025fc3510db2e2
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-08 23:22:55 +0500

    Add family type package

---
+++ family_types/__init__.py
@@ -0,0 +1,4 @@
+from .registry import FAMILY_TYPES, family_type_items, get_family_type
+from .strategies import infer_member_rules
+
+__all__ = ["FAMILY_TYPES", "family_type_items", "get_family_type", "infer_member_rules"]
