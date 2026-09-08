6aff8e06db7332448039e259dfb5e39895f3c8fb
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-09 00:01:47 +0500

    Expose kitchen base semantic parameter inference

--- family_types/kitchen_base.py
+++ family_types/kitchen_base.py
@@ -1,9 +1,14 @@
 from .casework_base import classify_role as _classify_casework_role
 from .casework_base import infer_rule as _casework_rule
+from .casework_base import infer_semantic_parameters as _infer_casework_parameters
 
 
 def classify_role(spans, centers, name=""):
     return _classify_casework_role(spans, centers, name=name)
+
+
+def infer_semantic_parameters(members, family_dims):
+    return _infer_casework_parameters(members, family_dims)
 
 
 def infer_rule(axis, span_ratio, center_ratio, name="", role=None):
