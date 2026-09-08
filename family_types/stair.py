f2435b37c5fb75540e8dc433e87799d688834edb
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-08 23:29:51 +0500

    Add stair family behavior module

---
+++ family_types/stair.py
@@ -0,0 +1,9 @@
+from .common import edge_member, name_has
+
+
+def infer_rule(axis, span_ratio, center_ratio, name=""):
+    if axis != "X":
+        return "FIXED"
+    if name_has(name, "stringer", "handrail") and edge_member(span_ratio, center_ratio):
+        return "MOVE"
+    return "STRETCH" if span_ratio >= 0.30 else "FIXED"
