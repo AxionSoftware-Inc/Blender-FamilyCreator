6460251b3346b2759b6fcb23a5fd4441774dbd5f
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-08 23:46:54 +0500

    Give windows dedicated semantic member roles

--- family_types/window.py
+++ family_types/window.py
@@ -1,5 +1,13 @@
+from .opening_base import classify_role as _classify_opening_role
 from .opening_base import infer_rule as _opening_rule
 
 
-def infer_rule(axis, span_ratio, center_ratio, name=""):
-    return _opening_rule(axis, span_ratio, center_ratio, name)
+ROLE_WINDOW_SASH = "WINDOW_SASH"
+
+
+def classify_role(spans, centers, name=""):
+    return _classify_opening_role(spans, centers, name=name, panel_role=ROLE_WINDOW_SASH)
+
+
+def infer_rule(axis, span_ratio, center_ratio, name="", role=None):
+    return _opening_rule(axis, span_ratio, center_ratio, name=name, role=role)
