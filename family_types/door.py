bd7ea97596ce1396b8f67113128ca92c3d604151
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-09 00:01:12 +0500

    Expose door semantic parameter inference

--- family_types/door.py
+++ family_types/door.py
@@ -1,5 +1,6 @@
 from .opening_base import classify_role as _classify_opening_role
 from .opening_base import infer_rule as _opening_rule
+from .opening_base import infer_semantic_parameters as _infer_opening_parameters
 
 
 ROLE_DOOR_LEAF = "DOOR_LEAF"
@@ -7,6 +8,10 @@ ROLE_DOOR_LEAF = "DOOR_LEAF"
 
 def classify_role(spans, centers, name=""):
     return _classify_opening_role(spans, centers, name=name, panel_role=ROLE_DOOR_LEAF)
+
+
+def infer_semantic_parameters(members, family_dims):
+    return _infer_opening_parameters(members, family_dims)
 
 
 def infer_rule(axis, span_ratio, center_ratio, name="", role=None):
