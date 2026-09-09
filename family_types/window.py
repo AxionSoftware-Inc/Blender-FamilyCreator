3d75e0ae8889d138b9d75ac5a928c235cb21c64f
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-09 11:16:08 +0500

    Refine generic window pane classification

--- family_types/window.py
+++ family_types/window.py
@@ -1,18 +1,2 @@
-from .opening_base import classify_role as _classify_opening_role
-from .opening_base import infer_rule as _opening_rule
-from .opening_base import infer_semantic_parameters as _infer_opening_parameters
-
-
-ROLE_WINDOW_SASH = "WINDOW_SASH"
-
-
-def classify_role(spans, centers, name=""):
-    return _classify_opening_role(spans, centers, name=name, panel_role=ROLE_WINDOW_SASH)
-
-
-def infer_semantic_parameters(members, family_dims):
-    return _infer_opening_parameters(members, family_dims)
-
-
-def infer_rule(axis, span_ratio, center_ratio, name="", role=None):
-    return _opening_rule(axis, span_ratio, center_ratio, name=name, role=role)
+from .opening_base import ROLE_GLASS
+from .opening_base import ROLE_WINDOW_SASH if False else None
