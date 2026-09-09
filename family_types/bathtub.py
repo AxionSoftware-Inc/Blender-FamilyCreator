390897e23efdb521959e0c7d6a8d591a80f53ff4
LEGION <137527004+AxionSoftware-Inc@users.noreply.github.com>
2026-09-09 05:10:54 +0500

    Add semantic bathtub roles

--- family_types/bathtub.py
+++ family_types/bathtub.py
@@ -1,5 +1,25 @@
-from .basin_base import infer_rule as _basin_rule
+from .basin_base import (
+    ROLE_TUB,
+    classify_role as _classify_role,
+    infer_rule as _basin_rule,
+    infer_semantic_parameters as _infer_semantic_parameters,
+)
 
 
-def infer_rule(axis, span_ratio, center_ratio, name=""):
-    return _basin_rule(axis, span_ratio, center_ratio, name, editable_axes=("X", "Y"))
+def classify_role(spans, centers, name=""):
+    return _classify_role(spans, centers, name=name, body_role=ROLE_TUB)
+
+
+def infer_rule(axis, span_ratio, center_ratio, name="", role=None):
+    return _basin_rule(
+        axis,
+        span_ratio,
+        center_ratio,
+        name,
+        editable_axes=("X", "Y"),
+        role=role,
+    )
+
+
+def infer_semantic_parameters(members, family_dims):
+    return _infer_semantic_parameters(members, family_dims, body_role=ROLE_TUB)
